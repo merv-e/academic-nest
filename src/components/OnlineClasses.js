f6f953ea37b075969824abcecb627e25d50ff78b
merv-e <34655454+merv-e@users.noreply.github.com>
2023-11-30 01:18:03 +0100

    Online classes component is created

---
+++ src/components/OnlineClasses.js
@@ -0,0 +1,43 @@
+import classesData from "../data";
+
+const OnlineClasses = () => {
+  return (
+    <section className="container-fluid mt-4 pb-5" id="online">
+      <h2>Explore Our Online Classes</h2>
+      <p>
+        Enroll in one of our online classes and embark on a journey of knowledge
+        and discovery.
+      </p>
+      <div className="row">
+        {classesData.map((item) => (
+          <div key={item.id} className="col-md-4 mb-4">
+            <div className="card">
+              <img
+                src={item.image}
+                alt={item.title}
+                className="card-img-top"
+                width="250"
+                height="300"
+                min-height="150"
+                min-width="100"
+              />
+              <div className="card-body">
+                <h5 className="card-title">{item.title}</h5>
+                <p className="card-text">{item.description}</p>
+                <a href="#enroll" className="btn btn-secondary">
+                  Enroll Now
+                </a>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="text-center mt-4">
+        <p>Couldn't find what you're looking for? Discover more options.</p>
+        <button className="btn btn-secondary">Discover More</button>
+      </div>
+    </section>
+  );
+};
+
+export default OnlineClasses;
---
+++ src/data.js
@@ -0,0 +1,53 @@
+import computer_science from "./assets/online-classes/computer-science.jpg";
+import history_of_art from "./assets/online-classes/history-of-art.jpg";
+import literature from "./assets/online-classes/literature.jpg";
+import mathematics from "./assets/online-classes/mathematics.jpg";
+import intro_to_psychology from "./assets/online-classes/mathematics.jpg";
+import phyton from "./assets/online-classes/mathematics.jpg";
+
+const classesData = [
+  {
+    id: 1,
+    title: "Introduction to Computer Science",
+    description:
+      "Dive into the fundamentals of computer science, exploring algorithms, programming languages, and problem-solving.",
+    image: computer_science,
+  },
+  {
+    id: 2,
+    title: "History of Art",
+    description:
+      "Embark on a visual journey through the ages, discovering the stories behind iconic works of art and their creators.",
+    image: history_of_art,
+  },
+  {
+    id: 3,
+    title: "Mathematics Fundamentals",
+    description:
+      "Uncover the beauty and logic of mathematics, from basic concepts to advanced problem-solving techniques.",
+    image: mathematics,
+  },
+  {
+    id: 4,
+    title: "English Literature",
+    description:
+      "Immerse yourself in the world of literature, exploring classic and contemporary works that shape the English literary landscape.",
+    image: literature,
+  },
+  {
+    id: 5,
+    title: "Introduction to Psychology",
+    description:
+      "Explore the human mind and behavior, delving into the foundations of psychology and key psychological theories.",
+    image: intro_to_psychology,
+  },
+  {
+    id: 6,
+    title: "Programming in Python",
+    description:
+      "Learn the fundamentals of Python programming, from syntax and data structures to building practical applications.",
+    image: phyton,
+  },
+];
+
+export default classesData;
